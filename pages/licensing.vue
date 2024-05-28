deb543ba7d3d9fb2c1c5cc1f688f8440e0ec84c0
EJ Fox <ejfox@ejfox.com>
2024-05-28 01:03:22 -0400

    chore: scaffold pages for prints, licensing

---
+++ pages/licensing.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-4xl font-bold mb-4">Licensing my photos</h1>
+    <p class="text-lg">My photos are available for licensing. Please contact me for more information.</p>
+    <h3 class="text-xl mt-4"><a href="mailto:ejfox@ejfox.com">ejfox@ejfox.com</a></h3>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
